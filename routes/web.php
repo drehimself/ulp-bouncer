363592c28c8998f0e72000391b05235a5da3058c
Andre Madarang <andre_madarang@hotmail.com>
2019-10-25 14:19:19 -0400

    working

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,27 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/posts/create', 'PostsController@create')->name('posts.create')->middleware('can:create,App\Post');
+    Route::post('/posts', 'PostsController@store')->name('posts.store')->middleware('can:create,App\Post');
+    Route::get('/posts/{post}/edit', 'PostsController@edit')->name('posts.edit')->middleware('can:update,post');
+    Route::patch('/posts/{post}', 'PostsController@update')->name('posts.update')->middleware('can:update,post');
+    Route::delete('/posts/{post}', 'PostsController@destroy')->name('posts.destroy')->middleware('can:delete,post');
+});
+
+Route::get('/posts', 'PostsController@index')->name('posts.index');
+// Route::get('/posts/create', 'PostsController@create')->name('posts.create');
+// Route::post('/posts', 'PostsController@store')->name('posts.store');
+Route::get('/posts/{post}', 'PostsController@show')->name('posts.show');
+// Route::get('/posts/{post}/edit', 'PostsController@edit')->name('posts.edit');
+// Route::patch('/posts/{post}', 'PostsController@update')->name('posts.update');
+// Route::delete('/posts/{post}', 'PostsController@destroy')->name('posts.destroy');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
